c4faff327a7836416ec30a3cd90eb68be1d4e31e
Ben Friedland <bfriedland@tiny13.local>
2015-08-08 12:12:35 -0700

    Some styling

---
+++ showertexts/util/texter.py
@@ -0,0 +1,24 @@
+import praw
+from twilio.rest import TwilioRestClient
+from custom_settings import ACCOUNT_SID, AUTH_TOKEN, SUBSCRIBERS
+
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+def main():
+    r = praw.Reddit(user_agent='shower_texts')
+    params = {}
+    all_time = False
+    if all_time:
+        params['t'] = 'all'
+    submissions = r.get_subreddit('showerthoughts').get_top(limit=1, params=params)
+    submission = submissions.next()
+    for number in SUBSCRIBERS:
+        client.messages.create(
+            to=number,
+            from_="+14152002895",
+            body=submission.title,
+        )
+
+if __name__ == '__main__':
+    main()
