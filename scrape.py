36be9f58b38ed55470142ad6f417bf40c9be9a3d
Ben Friedland <bfriedland@tiny13.local>
2015-08-07 20:37:56 -0700

    Subscriber support.

--- scrape.py
+++ scrape.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 import praw
 from twilio.rest import TwilioRestClient
-from settings import ACCOUNT_SID, AUTH_TOKEN
+from settings import ACCOUNT_SID, AUTH_TOKEN, SUBSCRIBERS
 
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 def main():
     r = praw.Reddit(user_agent='shower_texts')
-    submissions = r.get_subreddit('showerthoughts').get_top(limit=1, params = {'t': 'all'})
-    for submission in submissions:
-        print submission.title
+    submissions = r.get_subreddit('showerthoughts').get_top(limit=2)
+    #  params = {'t': 'all'}
+    submission = submissions.next()
+    submission = submissions.next()
+    for number in SUBSCRIBERS:
         client.messages.create(
-            to="2096223425",
+            to=number,
             from_="+14152002895",
             body=submission.title,
         )
