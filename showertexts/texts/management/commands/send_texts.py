0390abedbf641ffdb558814d4e4f33ecdb4ff126
Ben Friedland <ben174@gmail.com>
2015-08-08 19:05:46 -0700

    Remove scrape - undo settings hack

--- showertexts/texts/management/commands/send_texts.py
+++ showertexts/texts/management/commands/send_texts.py
@@ -1,14 +1,6 @@
-import os
-from django.conf import settings
-
 from django.core.management.base import BaseCommand
 from util.texter import send_todays_texts
 
-
-if 'DJANGO_ADMIN_PASSWORD' in os.environ:
-    settings.ACCOUNT_SID = os.environ['TWILIO_SID']
-    settings.AUTH_TOKEN = os.environ['TWILIO_TOKEN']
-    settings.ADMIN_PASSWORD = os.environ['DJANGO_ADMIN_PASSWORD']
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
