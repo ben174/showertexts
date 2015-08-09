dd03b224df5fd99c30ac13cad34fffeb74beaad6
Ben Friedland <ben174@gmail.com>
2015-08-08 18:54:45 -0700

    Cron and settings fixes.

--- showertexts/texts/management/commands/send_texts.py
+++ showertexts/texts/management/commands/send_texts.py
@@ -1,13 +1,14 @@
 import os
+from django.conf import settings
 
 from django.core.management.base import BaseCommand
 from util.texter import send_todays_texts
 
 
 if 'DJANGO_ADMIN_PASSWORD' in os.environ:
-    ACCOUNT_SID = os.environ['TWILIO_SID']
-    AUTH_TOKEN = os.environ['TWILIO_TOKEN']
-    ADMIN_PASSWORD = os.environ['DJANGO_ADMIN_PASSWORD']
+    settings.ACCOUNT_SID = os.environ['TWILIO_SID']
+    settings.AUTH_TOKEN = os.environ['TWILIO_TOKEN']
+    settings.ADMIN_PASSWORD = os.environ['DJANGO_ADMIN_PASSWORD']
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
