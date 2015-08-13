aab7c8d60446c7fc366b983f9ec387a63fde31d5
Ben Friedland <ben174@gmail.com>
2015-08-13 08:46:11 -0700

    Better admin

--- showertexts/texts/admin.py
+++ showertexts/texts/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from texts.models import Subscriber, TextSend, ShowerThought
 
-admin.site.register([Subscriber, TextSend, ShowerThought])
+
+
+class TextSendInline(admin.TabularInline):
+    exclude = ('hint_log', 'html', 'marks')
+    editable = False
+
+    model = TextSend
+
+@admin.register(ShowerThought)
+class ShowerThoughtAdmin(admin.ModelAdmin):
+    list_display = ('date', 'thought_text', 'post_id', 'active')
+    list_filter = ('date', 'active')
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('sms_number', 'date_created', 'active')
+    inlines = [
+        TextSendInline,
+    ]
+
+admin.site.register([TextSend, ])
